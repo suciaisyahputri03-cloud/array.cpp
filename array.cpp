a908c954fe2f0de9eb4cd8a6212be95b916d3d90
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 17:28:30 +0700

    menambhakna pengisian data dengan perintah satu persatu

--- array.cpp
+++ array.cpp
@@ -13,5 +13,16 @@ int main()
     //mengisi array
     dataku[1] = 200;
     cout << "isi data kedua setelah diubah : " << dataku[1] << endl;
-    
+
+    //mengisi data dengan perintah satu persatu
+    cout << "isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "isikan data kelima : ";
+    cin >> datamu[4];
 }

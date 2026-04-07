290655c8230e16c3b64e054f61d9e67fbb26df60
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 18:20:27 +0700

    menmabhakan deklarasi variable struct orang mhs dan mengisi data

--- struct.cpp
+++ struct.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 
+struct out_of_range
+{
+    string nama;
+    string alamat;
+    int umur;
+
+};
+
+int main()
+{
+    //deklarai variabel struct orang mhs;
+    // mengisi data 
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan ddata umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+}

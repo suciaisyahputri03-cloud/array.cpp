3a7a1d3af76bc5d01640c31a7702941e5a339b44
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 17:20:52 +0700

    masukkan mengisi array

--- array.cpp
+++ array.cpp
@@ -10,4 +10,8 @@ int main()
 
     //cara akses (membaca atau mengambil isi array)
     cout << "isi data kedua : " << dataku << endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah diubah : " << dataku[1] << endl;
+    
 }

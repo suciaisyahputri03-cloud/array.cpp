94f61e4e65951bb331eeae0bb368c8020f3cf2ba
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 18:52:46 +0700

    menambahkan  deklarasi variabel getline dan mengisi data

--- structgetline.cpp
+++ structgetline.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace  std;
+
+{
+     //deklarasi variable struct
+    orang msh;
+    // mengisi data
+    cout << "isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << " isikan data desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << " isikan data kota : ";
+    getline(cin, mhs.alamat.kota)
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "data tampil" << endl;
+    cout << "nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
+
+}

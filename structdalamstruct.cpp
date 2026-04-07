7ccceac255e7fa4f6baeb22b8249140e97703404
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 18:44:33 +0700

    input mengisi data

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -19,5 +19,21 @@ struct orang
  {
     //deklarasi variable struct
     orang msh;
-    
+    // mengisi data
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << " isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << " isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "data tampil" << endl;
+    cout << "nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
+
  }

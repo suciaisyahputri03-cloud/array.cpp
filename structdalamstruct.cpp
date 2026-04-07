e9fc1dfeb92a1be666005a9153573af615cdbcd4
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-07 18:34:25 +0700

    menambhakan struct detail alamat dan struct orang

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+
+
+};
+
+struct orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+ int main ()
+ {
+    
+ }
